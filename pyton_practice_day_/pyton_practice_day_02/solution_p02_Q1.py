e2018ce0f47862b284e61ca74d100414aaae0fcc
sansa <iyui5582@gmail.com>
2026-01-16 21:43:44 +0900

    2026/01/16

---
+++ pyton_practice_day_/pyton_practice_day_02/solution_p02_Q1.py
@@ -0,0 +1,11 @@
+num1 = a
+num2 = b
+input(a)
+input(b)
+    print(f"a * b = {a * b}");
+#ㄴ아예 접근을 잘못함. df사용할 생각을 못함.
+
+def solution(num1, num2):
+    answer = num1 * num2
+    return answer;
+    
---
+++ pyton_practice_day_/pyton_practice_day_02/solution_p02_Q2.py
@@ -0,0 +1,4 @@
+price = 3000
+count = 3
+print(price * count);
+#ㄴ설명이 필요없다...
